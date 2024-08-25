14251d96a2c19a1e1f71fd73814c7f0f074facb8
Matheus Andrade <125470760+MatAndrade01@users.noreply.github.com>
2024-08-25 18:00:25 -0300

    Estilizando o componente CardeColaboradores

--- src/components/CardColaborador/CardColaborador.js
+++ src/components/CardColaborador/CardColaborador.js
@@ -1,12 +1,15 @@
 import './styles.css'
 
-const CardColaborador = () => {
+const CardColaborador = ({src, childrenName, childrenDescrition}) => {
     return (
       <>
         <div className='cardContainer'>
             <div className='topCard'>
+              <img src={src} alt='Foto colaborador'></img>
             </div>
             <div className='bottomCard'>
+              <h4>{childrenName}</h4>
+              <p>{childrenDescrition}</p>
             </div>
         </div>
       </>
